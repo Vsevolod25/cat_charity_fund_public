8be62dab94c0ccf375fbeab1e2076ae53e82ad8f
Vsevolod Kolupatin <vsevolod25@yandex.ru>
2024-07-01 02:25:06 +0400

    Remove unnecessary variables

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -5,22 +5,21 @@ from aiogoogle import Aiogoogle
 
 from app.models import CharityProject
 from .constants import (
-    BASE_TABLE_VALUES,
     COLUMN_COUNT,
     DATETIME_FORMAT,
     PERMISSIONS_BODY,
     ROW_COUNT,
-    SPREADSHEET_BODY
+    SPREADSHEET_BODY,
+    TABLE_VALUES
 )
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> Tuple[str]:
     now_date_time = datetime.now().strftime(DATETIME_FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    spreadsheet_body = SPREADSHEET_BODY
-    spreadsheet_body['properties']['title'] = f'Отчёт от {now_date_time}'
+    SPREADSHEET_BODY['properties']['title'] = f'Отчёт от {now_date_time}'
     response = await wrapper_services.as_service_account(
-        service.spreadsheets.create(json=spreadsheet_body)
+        service.spreadsheets.create(json=SPREADSHEET_BODY)
     )
     return response['spreadsheetId'], response['spreadsheetUrl']
 
@@ -45,22 +44,15 @@ async def spreadsheets_update_value(
 ) -> Union[None, ValueError]:
     now_date_time = datetime.now().strftime(DATETIME_FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    table_values = BASE_TABLE_VALUES
-    # Переменную table_values нужно объявить,
-    # чтобы добавить дату и время отчета, не меняя саму константу
-    # (как и в строках 22-23)
-    table_values[0] = ['Отчёт от', now_date_time]
+    TABLE_VALUES[0] = ['Отчёт от', now_date_time]
     if time_projects:
         for project in time_projects:
-            table_values.append(
+            TABLE_VALUES.append(
                 [project[1].name, project[0], project[1].description]
             )
-    else:
-        table_values[1] = ['Нет информации о закрытых проектах']
-        table_values.pop(2)
 
-    table_rows = len(table_values)
-    table_columns = len(table_values[-1])
+    table_rows = len(TABLE_VALUES)
+    table_columns = len(TABLE_VALUES[-1])
     if table_rows > ROW_COUNT or table_columns > COLUMN_COUNT:
         raise ValueError(
             'Таблица недостаточного размера для отображения проектов. '
@@ -70,7 +62,7 @@ async def spreadsheets_update_value(
 
     update_body = {
         'majorDimension': 'ROWS',
-        'values': table_values
+        'values': TABLE_VALUES
     }
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
